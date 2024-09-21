66bd96793e7c912a8303aa8e54284e13711acf52
robiuzzaman4 <robiuzzaman4@gmail.com>
2024-09-21 22:07:51 +0600

    feat: initial project setup done

---
+++ README.md
@@ -0,0 +1,1 @@
+## **Recursive Comment Tree**
---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+const App = () => {
+  return (
+    <section>
+      <h1 className="text-3xl font-medium">Hello React</h1>
+    </section>
+  );
+};
+
+export default App;
