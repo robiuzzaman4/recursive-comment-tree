f77e5a6be67690a81ae029e814790979192b490f
robiuzzaman4 <robiuzzaman4@gmail.com>
2024-09-21 23:00:47 +0600

    style: update font and other styles

--- src/App.tsx
+++ src/App.tsx
@@ -38,8 +38,14 @@ const initialComments: TComment[] = [
 
 const CommentItem = ({ comment }: TCommentItem) => {
   return (
-    <div className="p-2 rounded-[1px] border border-neutral-700 bg-neutral-800">
-      <p>{comment.text}</p>
+    <div className="p-2 rounded-sm border border-neutral-700 bg-neutral-800 flex flex-col gap-2">
+      <p className="text-neutral-300">{comment.text}</p>
+
+      <div className="flex items-center gap-2">
+        <Button size="sm">Edit</Button>
+        <Button size="sm">Delete</Button>
+        <Button size="sm">Reply</Button>
+      </div>
     </div>
   );
 };
@@ -48,14 +54,14 @@ const App = () => {
   const [comments, setComments] = useState<TComment[]>(initialComments);
   return (
     <main className="min-h-screen w-full bg-neutral-900 text-white flex flex-col gap-12">
-      <section className="w-full max-w-screen-md mx-auto px-4 py-12">
+      <section className="w-full max-w-screen-sm mx-auto px-4 py-12">
         {/* title */}
-        <h3 className="text-2xl font-normal tracking-tighter mb-6">
+        <h3 className="text-2xl font-normal tracking-tighter mb-6 bg-clip-text text-transparent bg-gradient-to-r from-neutral-300 via-sky-500 to-neutral-300">
           Recursive Comment Tree:
         </h3>
 
         <div className="w-full flex items-center gap-6 pb-6">
-          <input className="w-full px-4 py-2 rounded-[1px] bg-neutral-800 border border-neutral-700 focus-visible:outline-none text-sm text-neutral-400" />
+          <input className="w-full px-4 py-2 rounded-sm bg-neutral-800 border border-neutral-700 focus-visible:outline-none text-sm text-neutral-400" />
           <Button>Comment</Button>
         </div>
 
--- src/components/button.tsx
+++ src/components/button.tsx
@@ -4,15 +4,17 @@ import { cn } from "../utils/cn";
 type TButton = {
   children: ReactNode;
   className?: string;
-  size?: "xs" | "sm";
+  size?: "sm";
 };
 
-const Button = ({ children, className }: TButton) => {
+const Button = ({ children, className, size }: TButton) => {
   return (
     <button
       className={cn(
-        "px-4 py-2 rounded-[1px] inline-flex items-center justify-center text-sm font-medium bg-neutral-800 hover:bg-neutral-700 text-neutral-200 border border-neutral-700",
-        className
+        "px-4 py-2 rounded-sm inline-flex items-center justify-center text-sm font-normal bg-neutral-800 hover:bg-neutral-700 text-neutral-300 border border-neutral-700",
+        className, {
+            "px-2 py-0.5 text-xs rounded-full" : size === "sm"
+        }
       )}
     >
       {children}
